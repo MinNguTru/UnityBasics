b5e73b9ce0eea643b2575cf4616b584f33444785
MinNguTru <85091785+MinNguTru@users.noreply.github.com>
2023-04-01 12:51:40 -0700

    Add files via upload

---
+++ PatrolPath/Assets/PatrolPath.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PatrolPath : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int current = 0;
+    public float speed;
+    private float WPradius = 2f;
+
+    private void Update()
+    {
+        if (Vector2.Distance(waypoints[current].transform.position, transform.position) < WPradius)
+        {
+            current++;
+            if (current >= waypoints.Length)
+            {
+                current = 0;
+            }
+
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    }
+}
